06713659d5c6d150675abe30663e9671c3e510b6
Harshit Garg <harshitgargmnit@gmail.com>
2019-04-12 23:54:06 +0530

    Add pagination in Inventory list API

--- inventory/scripts/load_inventory.py
+++ inventory/scripts/load_inventory.py
@@ -1,3 +1,23 @@
 import requests
+import time
+import random
+
+def make_purchase():
+	pass
 
 
+def get_list():
+	url = "http://127.0.0.1:8000/inventory/list/?"
+	params = "format=json&page=10&per_page=10"
+
+	response = requests.get(url + "?" + params)
+
+	print response.content
+
+while True:
+	if random.randint(0, 1) % 2 == 1:
+		get_list()
+	else:
+		make_purchase()
+
+	time.sleep(10)
--- inventory/views.py
+++ inventory/views.py
@@ -8,6 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics, status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from django.core.paginator import Paginator
+
 
 from .models import InventoryItem
 from .serializers import InventoryItemSerializer
@@ -37,10 +39,22 @@ class InventoryItemListGenerics(generics.ListCreateAPIView):
     serializer_class = InventoryItemSerializer
     pagination_class = PageNumberPagination
 
-    #TODO: pagination
+    def get_paginated_response(self, page=1, per_page=10):
+        paginator = Paginator(self.queryset, per_page)
+        return paginator.page(page)
+
 
     def list(self, request):
-        print self.request.GET
-        queryset = self.get_queryset()
-        serializer = InventoryItemSerializer(queryset, many=True)
-        return Response(serializer.data)
+        page = self.request.GET.get('page')
+        per_page = self.request.GET.get('per_page')
+
+        paginated_queryset = self.get_paginated_response(page, per_page)
+        serializer = InventoryItemSerializer(paginated_queryset, many=True)
+        json = {
+            'page': page,
+            'data': serializer.data,
+            'per_page': request.GET['per_page'],
+            'total': paginated_queryset.end_index()-paginated_queryset.start_index()+1
+        }
+
+        return Response(json)
