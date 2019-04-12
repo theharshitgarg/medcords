c125ccf2f0c474d7bb84dda9dde41927d639972d
Harshit Garg <harshitgargmnit@gmail.com>
2019-04-12 21:24:16 +0530

    Add Inventory Items

--- inventory/models.py
+++ inventory/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class InventoryItem(models.Model):
+	quantity = models.IntegerField(default=0)
+	name = models.CharField(max_length=100)
+
+
+	def __unicode__(self):
+		return self.name + " : " + self.quantity
+
+	def is_available(self):
+		return self.quantity > 0
+
+	
