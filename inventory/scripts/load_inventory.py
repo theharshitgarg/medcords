81f90759b0d70d3f0e61eeff2404b79fcec96f3f
Harshit Garg <harshitgargmnit@gmail.com>
2019-04-13 08:52:53 +0530

    Add client script for making purchase

--- inventory/scripts/load_inventory.py
+++ inventory/scripts/load_inventory.py
@@ -3,12 +3,21 @@ import time
 import random
 
 def make_purchase():
-	pass
+	data = {
+		"items": [
+			{ "id": 1, "quantity": 100 },
+			{ "id": 10, "quantity": 30 },
+			{ "id": 100, "quantity": 10 },
+		]
+	}
+	url = "http://127.0.0.1:8000/inventory/purchase"
+	response = requests.post(url, json=data)
+	print response.content
 
 
 def get_list():
 	url = "http://127.0.0.1:8000/inventory/list/?"
-	params = "format=json&page=10&per_page=10"
+	params = "format=json&page=10&per_page=" + str(random.randint(1, 20))
 
 	response = requests.get(url + "?" + params)
 
